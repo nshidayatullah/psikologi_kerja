e5b2d16834b42b12252f4d87616c7e1830486ab4
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2026-03-06 22:22:31 +0800

    Add main admin user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin Psikologi',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $this->call([
+            QuestionSeeder::class,
+            SignerSeeder::class,
         ]);
     }
 }
