401789a0d3b59de412bf9ea40d0e9cbc0d6d2583
nshidayatullah <127877651+nshidayatullah@users.noreply.github.com>
2026-03-06 22:27:36 +0800

    Implement FilamentUser to fix 403 Forbidden in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Allow all users to access the panel for now (since we use Spatie permissions inside)
+    }
 
     /**
      * The attributes that are mass assignable.
